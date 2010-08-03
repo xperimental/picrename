7e2a4620ee0ddd0de0af71ca30c198a5577c8ab7
Robert <xperimental@solidproject.de>
2010-08-03 17:19:52 +0300

    Created basic project.

---
+++ src/test/java/net/sourcewalker/picrename/AppTest.java
@@ -0,0 +1,14 @@
+package net.sourcewalker.picrename;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void noopTest() {
+        assertTrue(true);
+    }
+
+}
